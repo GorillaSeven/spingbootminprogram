6460a857a1cba07af1aa4f5879fa38ffbf794702
1135459639@qq.com <1135459639@qq.com>
2019-05-24 13:18:40 +0800

    提交src

---
+++ minprogram/src/main/java/com/jiangdong/minprogram/MinProgramApplication.java
@@ -0,0 +1,13 @@
+package com.jiangdong.minprogram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinProgramApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MinProgramApplication.class, args);
+    }
+
+}
